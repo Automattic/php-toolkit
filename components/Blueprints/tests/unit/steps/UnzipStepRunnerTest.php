9ff8f5d0dbc1da641e862b7dcaa8887e7eb5faeb
Adam Zieliński <adam.zielinski@automattic.com>
2025-04-30 17:54:13 +0200

    Test UnzipStep

--- components/Blueprints/tests/unit/steps/UnzipStepRunnerTest.php
+++ components/Blueprints/tests/unit/steps/UnzipStepRunnerTest.php
@@ -2,31 +2,34 @@
 
 namespace unit\steps;
 
-use PHPUnit\Framework\MockObject\MockObject;
-use PHPUnit\Framework\MockObject\Stub;
+use InvalidArgumentException;
 use PHPUnitTestCase;
-use Symfony\Component\Filesystem\Filesystem;
-use Symfony\Component\Filesystem\Path;
 use WordPress\Blueprints\Model\DataClass\UnzipStep;
 use WordPress\Blueprints\Progress\Tracker;
-use WordPress\Blueprints\Resources\ResourceManager;
+use WordPress\Blueprints\Resources\Model\DataReference;
+use WordPress\Blueprints\Resources\Model\File;
 use WordPress\Blueprints\Runner\Step\UnzipStepRunner;
 use WordPress\Blueprints\Runtime\Runtime;
+use WordPress\Filesystem\Filesystem;
+use WordPress\Filesystem\FilesystemException;
+use WordPress\Filesystem\LocalFilesystem;
+use WordPress\Zip\ZipFilesystem;
+
+use function WordPress\Filesystem\wp_join_paths;
 
 class UnzipStepRunnerTest extends PHPUnitTestCase {
-
 	/**
-	 * @var string $document_root
+	 * @var string
 	 */
 	private $document_root;
 
 	/**
-	 * @var Runtime $runtime
+	 * @var Runtime|\PHPUnit\Framework\MockObject\MockObject
 	 */
 	private $runtime;
 
 	/**
-	 * @var UnzipStepRunner $step_runner
+	 * @var UnzipStepRunner
 	 */
 	private $step_runner;
 
@@ -36,65 +39,54 @@ class UnzipStepRunnerTest extends PHPUnitTestCase {
 	private $filesystem;
 
 	/**
-	 * @var ResourceManager&MockObject $resource_manager resource manager mock
+	 * @var Tracker
 	 */
-	private $resource_manager;
+	private $progress_tracker;
 
 	/**
 	 * @before
 	 */
-	public function before() {
-		$this->document_root = Path::makeAbsolute( 'test', sys_get_temp_dir() );
-		$this->runtime       = new Runtime( $this->document_root );
+	public function setUp(): void {
+		$this->document_root = wp_join_paths(sys_get_temp_dir(), 'test_' . uniqid());
+		$this->filesystem = LocalFilesystem::create($this->document_root);
 
-		$this->resource_manager = $this->createMock( ResourceManager::class );
+		$this->runtime       = new Runtime( $this->document_root );
+		copy(
+			wp_join_paths( __DIR__, 'resources', 'test_zip.zip' ),
+			wp_join_paths( $this->document_root, 'test_zip.zip' )
+		);
+
+		$this->progress_tracker = new Tracker();
 
 		$this->step_runner = new UnzipStepRunner();
-		$this->step_runner->setRuntime( $this->runtime );
-		$this->step_runner->setResourceManager( $this->resource_manager );
-
-		$this->filesystem = new Filesystem();
+		$this->step_runner->setRuntime($this->runtime);
 	}
 
 	/**
 	 * @after
 	 */
-	public function after() {
-		$this->filesystem->remove( $this->document_root );
+	public function tearDown(): void {
+		try {
+			if ($this->filesystem->exists('/')) {
+				$this->filesystem->rmdir('/', [
+					'recursive' => true
+				]);
+			}
+		} catch (\Exception $e) {
+			// Ignore cleanup errors
+		}
 	}
 
-	public function testUnzipFileWhenUsingAbsolutePath() {
-		// TODO: Remove once UnzipStep is implemented
-		$this->markTestSkipped( 'UnzipStep not yet implemented.' );
-
-		$zip = __DIR__ . '/resources/test_zip.zip';
-		$this->resource_manager->method( 'getStream' )
-			->willReturn( fopen( $zip, 'rb' ) );
-
+	public function testRunWithValidDataReference() {
+		// Create and run the step
 		$step = new UnzipStep();
-		$step->setZipFile( $zip );
-		$extracted_file_path = $this->runtime->resolvePath( 'dir/test_zip.txt' );
-		$step->setExtractToPath( Path::getDirectory( $extracted_file_path ) );
+		$step->zipFile = DataReference::from_json('./test_zip.zip');
+		$step->extractToPath = 'extract_dir';
+		
+		$this->step_runner->run($step, $this->progress_tracker);
 
-		$this->step_runner->run( $step, new Tracker() );
-		self::assertFileEquals( __DIR__ . '/resources/test_zip.txt', $extracted_file_path );
+		$this->assertTrue($this->filesystem->exists('extract_dir'));
+		$this->assertTrue($this->filesystem->exists('extract_dir/test_zip.txt'));
 	}
 
-	public function testUnzipFileWhenUsingRelativePath() {
-		// TODO: Remove once UnzipStep is implemented
-		$this->markTestSkipped( 'UnzipStep not yet implemented.' );
-
-		$zip = __DIR__ . '/resources/test_zip.zip';
-		$this->resource_manager->method( 'getStream' )
-			->willReturn( fopen( $zip, 'rb' ) );
-
-		$step = new UnzipStep();
-		$step->setZipFile( $zip );
-		$step->setExtractToPath( 'dir' );
-
-		$this->step_runner->run( $step, new Tracker() );
-
-		$extracted_file_path = $this->runtime->resolvePath( 'dir/test_zip.txt' );
-		self::assertFileEquals( __DIR__ . '/resources/test_zip.txt', $extracted_file_path );
-	}
 }
