fc670cf88f33265a5fa2dfacf8606a02b643eae9
Adam Zieliński <adam.zielinski@automattic.com>
2025-05-23 17:31:05 +0200

    Restructure comments

--- components/HttpClient/Client/SocketClient.php
+++ components/HttpClient/Client/SocketClient.php
@@ -12,59 +12,15 @@ use WordPress\HttpClient\Request;
 use WordPress\HttpClient\Response;
 
 /**
- * An asynchronous HTTP client library.
- *
- * This class makes non-blocking HTTP requests using just native PHP streams.
- * It uses an event-driven architecture.
- *
- * ### Key Features
- *
- * - **Streaming support:** Enables efficient handling of large response bodies.
- * - **Progress monitoring:** Track the progress of requests and responses.
- * - **Concurrency limits:** Control the number of simultaneous connections.
- * - **PHP 7.2+ support and no dependencies:** Works on vanilla PHP without external libraries.
- *
- * ### Usage Example
- *
- * ```php
- * $requests = [
- *     new Request("[https://wordpress.org/latest.zip](https://wordpress.org/latest.zip)"),
- *     new Request("[https://raw.githubusercontent.com/wpaccessibility/a11y-theme-unit-test/master/a11y-theme-unit-test-data.xml](https://raw.githubusercontent.com/wpaccessibility/a11y-theme-unit-test/master/a11y-theme-unit-test-data.xml)"),
- * ];
- *
- * $client = new Client();
- * $client->enqueue($requests);
- *
- * while ($client->await_next_event()) {
- *     $event = $client->get_event();
- *     $request = $client->get_request();
- *
- *     if ($event === Client::EVENT_BODY_CHUNK_AVAILABLE) {
- *         $chunk = $client->get_response_body_chunk();
- *         // Process the chunk...
- *     }
- *     // Handle other events...
- * }
- * ```
- *
- * @TODO
- * * Request headers – accept string lines such as "Content-type: text/plain" instead of key-value pairs. K/V pairs
- *   are confusing and lead to accidental errors such as `0: Content-type: text/plain`. They also diverge from the
- *   format that curl accepts.
- * * Response caching – add a custom cache handler for easy caching of the same URLs
- * * Response caching – support HTTP cache-control headers
+ * An HTTP client using stream_socket_client(). Supports
+ * concurrent connections just like curl_multi.
  */
 class SocketClient extends Client {
 
 	protected const STREAM_SELECT_READ = 1;
 	protected const STREAM_SELECT_WRITE = 2;
 
-	/**
-	 * Asynchronously moves the enqueued Request objects through the
-	 * various states of the HTTP request-response lifecycle.
-	 *
-	 * @return bool Whether any active requests were processed.
-	 */
+
 	protected function event_loop_tick() {
 		if ( count( $this->get_active_requests() ) === 0 ) {
 			return false;
